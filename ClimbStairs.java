f0f717753d241eb84d397fd2eaa467e98226a867
Siddharth Kumar <roysiddharth458@gmail.com>
2025-04-10 15:25:20 +0530

    You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?

---
+++ ClimbStairs.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbStairs {
+
+    public static int myFunc(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
+    }
+    
+    private static int climbStairs(int n, Map<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
+        }
+        return memo.get(n);
+    }
+    public static void main(String[] args) {
+        System.out.println(myFunc(3));
+
+    }
+}
